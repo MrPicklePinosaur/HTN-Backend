a63bb482ea54127b284af5f42f4e828fbf6acd72
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-03-09 18:45:17 -0500

    mutation

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,6 +1,23 @@
-import { Arg, Query, Resolver } from 'type-graphql';
-import { User } from './types/User.types';
+import { Arg, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
+import { Skill, User } from './types/User.types';
 import { dummyUsers } from './services/db.service';
+
+/* bit of code duplication, sorta ugly */
+@InputType()
+class UpdateUserInput implements Partial<User> {
+
+    @Field({ nullable: true })
+    picture?: String;
+
+    @Field({ nullable: true })
+    company?: String;
+
+    @Field({ nullable: true })
+    email: String;
+
+    @Field({ nullable: true })
+    phone: String;
+}
 
 @Resolver()
 export class UserResolver {
@@ -17,4 +34,24 @@ export class UserResolver {
 
         return filtered;
     }
+
+    @Mutation(() => String)
+    async updateUser(
+        @Arg("id", {}) id: number,
+        @Arg('newdata', {}) newdata: UpdateUserInput
+    ) {
+        var updateuser: User|undefined = this.userCollection.find(user => user.id === id);
+
+        if (updateuser != undefined) {
+            console.log( {
+                ...updateuser,
+                ...newdata
+            });
+        }
+
+        return "updated";
+
+        // do some error stuff
+    }
+
 }
