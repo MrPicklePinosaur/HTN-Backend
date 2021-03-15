651fae6d3fa5492fa877195fd643d67056fd6355
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-03-15 15:45:47 -0400

    mrogan

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import express from 'express';
+import morgan from 'morgan';
 
 import { initDB } from './services/db.service';
 import { UserResolver } from './resolvers/User.resolver';
 import { SkillResolver } from './resolvers/Skill.resolver';
 import { UserSkillResolver } from './resolvers/UserSkill.resolver';
-
 
 const main = async () => {
     await initDB();
@@ -19,6 +19,7 @@ const main = async () => {
 
     const apollo = new ApolloServer({ schema });
     const app = express();
+    app.use(morgan('combined'));
     apollo.applyMiddleware({ app });
 
     app.listen(3000, () => {
